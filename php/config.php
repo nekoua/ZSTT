41234006c1a759163a54428ec3849a904bd8e297
nekoua <s.kotljarov@gmail.com>
2018-07-12 19:49:55 +0300

    Revert "zd"

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = "root"; //mysql username
-$password = "pass"; //mysql password
-$hostname = "localhost"; //hostname
-$databasename = "employee"; //databasename
-
-$connecDB = mysql_connect($hostname, $username, $password)or die('could not connect to database');
-mysql_select_db($databasename,$connecDB);
-?>
